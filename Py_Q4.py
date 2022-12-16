7a082f4b9f9d542c4375370f017bff7de0ed1dde
AshutoshKumarPande <104226144+AshutoshKumarPande@users.noreply.github.com>
2022-12-17 00:26:58 +0530

    Add files via upload
    Digits of a number

---
+++ Py_Q4.py
@@ -0,0 +1,16 @@
+# Program Created by:- Ashutosh Kumar Pandey
+# Program created on:- 5th November, 2022
+
+def digits(n):
+    list = []
+    while(n>0):
+        digit=n%10
+        list.append(digit)
+        n=n//10
+    return set(list)
+n = int(input("Enter a number : "))
+nls = digits(n)
+print(nls)
+print("The digits of number as a set")
+for x in nls:
+    print(x,end=" ")
